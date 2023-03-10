1edeabf7053d162853ed95edf09c0ad8f1979db9
Aezlan <120666507+Aezlan@users.noreply.github.com>
2023-03-10 11:57:25 -0500

    Create success.php

---
+++ success.php
@@ -0,0 +1,12 @@
+//success.php
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Login Success</title>
+  </head>
+  <body>
+    <h1>Login Success!</h1>
+    <p>Click <a href="index.html">here</a> to return home.</p>
+  </body>
+</html>
