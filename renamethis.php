b833735a699b7b493488415274583074067b084e
Aezlan <120666507+Aezlan@users.noreply.github.com>
2023-03-10 12:05:45 -0500

    Create renamethis.php

---
+++ renamethis.php
@@ -0,0 +1,51 @@
+//“renamethis”.php
+//This is your Posts, rename to fit your needs
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>edit this</title>
+    <style>
+      /* Add some basic styles */
+      body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+        background-color: #f5f5f5;
+      }
+      
+      header {
+        background-color: #333;
+        color: #fff;
+        padding: 20px;
+        text-align: center;
+      }
+      
+      h1 {
+        margin: 0;
+        font-size: 36px;
+        letter-spacing: 2px;
+      }
+      
+      main {
+        margin: 20px;
+        padding: 20px;
+        background-color: #fff;
+        box-shadow: 0px 0px 5px rgba(0,0,0,0.3);
+      }
+      
+      p {
+        font-size: 18px;
+        line-height: 1.5;
+      }
+    </style>
+  </head>
+  <body>
+    <header>
+      <h1>Post 4</h1>
+    </header>
+    <main>
+      <p>This is the content of Post (edit this)</p>
+    </main>
+  </body>
+</html>
