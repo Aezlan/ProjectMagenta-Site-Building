1e6b078c595452dcb2884d2eafd8d7730a6a16d4
Aezlan <120666507+Aezlan@users.noreply.github.com>
2023-03-10 11:58:10 -0500

    Create failed.php

---
+++ failed.php
@@ -0,0 +1,12 @@
+//failed.php
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Login Failed</title>
+  </head>
+  <body>
+    <h1>Login Failed</h1>
+    <p>Click <a href="login.php">here</a> to try again.</p>
+  </body>
+</html>
